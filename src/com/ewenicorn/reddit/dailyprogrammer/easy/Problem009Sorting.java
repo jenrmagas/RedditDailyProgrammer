7d2700ec7abd252f280d10188208b4ef1769661d
Jen Magas <jen.r.magas@gmail.com>
2013-04-03 22:31:22 -0500

    Solutions for problems 9 and 11 (easy)

---
+++ src/com/ewenicorn/reddit/dailyprogrammer/easy/Problem009Sorting.java
@@ -0,0 +1,39 @@
+package com.ewenicorn.reddit.dailyprogrammer.easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * http://redd.it/pu1rf
+ * 
+ * Write a program that will allow the user to input digits, and arrange them in numerical order.
+ * for extra credit, have it also arrange strings in alphabetical order
+ * 
+ */
+public class Problem009Sorting {
+
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in));
+		List<Integer> items = new ArrayList<Integer>();
+		
+		try {
+			System.out.print("Enter a list of values to be sorted (separated by space): ");
+			
+			String rawList = reader.readLine();
+			for(String item : rawList.split(" ")) {
+				items.add(Integer.valueOf(item));
+			}
+			
+			Collections.sort(items);
+			System.out.println(items);
+		} catch (IOException e) {
+			System.out.println(e.getStackTrace());
+		}
+		
+	}
+}
---
+++ src/com/ewenicorn/reddit/dailyprogrammer/easy/Problem011DayOfTheWeek.java
@@ -0,0 +1,45 @@
+package com.ewenicorn.reddit.dailyprogrammer.easy;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * http://redd.it/pwons
+ * 
+ * The program should take three arguments. The first will be a day, the second will be month, 
+ * and the third will be year. Then, your program should compute the day of the week that date 
+ * will fall on.
+ * 
+ */
+public class Problem011DayOfTheWeek {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(
+				new InputStreamReader(System.in));
+		SimpleDateFormat formatter = new SimpleDateFormat("EEEE");
+		int day, mon, year;
+		Calendar targetDate;
+		try {
+			System.out.println("Based on the day, month, and year provided, you can find out what day of the week it is.\n");
+
+			System.out.print("Enter the year (format of YYYY): ");
+			year = Integer.valueOf(reader.readLine());
+			System.out.print("Enter the month (format of MM): ");
+			mon = Integer.valueOf(reader.readLine()) - 1; // Accommodate zero-based months
+			System.out.print("Enter the day (format of D or DD): ");
+			day = Integer.valueOf(reader.readLine());
+			
+			targetDate = new GregorianCalendar(year, mon, day);
+			System.out.println("The date provided occurred on a " + formatter.format(targetDate.getTime()));
+		} catch(Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+
+}
