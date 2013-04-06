8a5c5f50defa0181b64b3db3265111d0f3b47d92
Jen Magas <jen.r.magas@gmail.com>
2013-04-06 09:51:07 -0500

    Solution for problem 13 (easy)

---
+++ src/com/ewenicorn/reddit/dailyprogrammer/easy/Problem013DayOfYear.java
@@ -0,0 +1,25 @@
+package com.ewenicorn.reddit.dailyprogrammer.easy;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Calendar;
+
+public class Problem013DayOfYear {
+
+	public static void main(String[] args) {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(
+				System.in));
+
+		try {
+			System.out.print("To determine the day of the year, enter the date in the format of YYYY/MM/DD: ");
+			String[] rawDate = reader.readLine().split("/");
+			Calendar cal = Calendar.getInstance();
+			cal.clear();
+			cal.set(Integer.valueOf(rawDate[0]), Integer.valueOf(rawDate[1])-1, Integer.valueOf(rawDate[2]));
+			System.out.println("The provided date is day " + cal.get(Calendar.DAY_OF_YEAR) + " of the year.");
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
